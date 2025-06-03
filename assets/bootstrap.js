c6ff730ee4ce1403718cc3b9ab52693448f7154e
fatma <itchirfatma@gmail.com>
2025-06-04 01:15:31 +0200

    ajouter user entity, ajouter webpack, ajouter bundle néccessaire

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -1,5 +1,8 @@
-import { startStimulusApp } from '@symfony/stimulus-bundle';
+import { Application } from '@hotwired/stimulus';
 
-const app = startStimulusApp();
-// register any custom, 3rd party controllers here
+const app = Application.start();
+
+// Ici tu peux enregistrer tes controllers Stimulus custom, par exemple :
 // app.register('some_controller_name', SomeImportedController);
+
+export default app;
